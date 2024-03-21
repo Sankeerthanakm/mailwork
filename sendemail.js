0993db571b4895aed197825e4a3c523411304be5
Sankeerthanakm <146733173+Sankeerthanakm@users.noreply.github.com>
2024-03-21 22:02:48 +0530

    mail

--- sendemail.js
+++ sendemail.js
@@ -1,27 +1,27 @@
-// Import nodemailer
+
 const nodemailer = require('nodemailer');
 
-// Create a transporter object using SMTP transport
+
 let transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'apmajmal52@gmail.com', // Your Gmail email address
-        pass: 'svpl qiaz lpwp knos' // Your Gmail password or app-specific password
+        user: 'sankeerthana2000km@gmail.com', 
+        pass: 'vfyk nyvu paqe egfx' 
     },
     tls:{
         rejectUnauthorized: false
     }
 });
 
-// Define email options
+
 let mailOptions = {
-    from: 'apmajmal52@gmail.com',
-    to: 'apmajmal52@gmail.com', // Friend's email address
+    from: 'sankeerthana2000km@gmail.com',
+    to: 'sankeerthana2000km@gmail.com', 
     subject: 'Test Email',
     text: 'This is a test email sent using Nodemailer.'
 };
 
-// Send mail with defined transport object
+
 transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
         return console.log('Error occurred:', error);
